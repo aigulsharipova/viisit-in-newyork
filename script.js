bc0bb5c4ede371501e1344d071737777937e7355
Aigul <sharipova.aig@gmail.com>
2022-10-11 18:50:58 -0400

    function first

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const text = document.querySelector('h2');
 
 const btnOne = document.querySelector('#one');
 const btnTwo = document.querySelector('#two');
+
+btnOne.addEventListener('click', first);
+btnTwo.addEventListener('click', noFirst);
+
+function first() {
+  image.setAttribute("src", "./style/photo2.webp");
+  text.innerHTML = "Did you see all popular places?";
+  btnOne.innerHTML = "Yes!";
+  btnTwo.innerHTML = "No!";
+
+  btnOne.addEventListener('click', allPopular);
+  btnTwo.addEventListener('click', notPopular);
+}
