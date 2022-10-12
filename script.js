ba21471ef04e7a49b0fb06c8526b431981bb7243
Aigul <sharipova.aig@gmail.com>
2022-10-11 22:36:00 -0400

    function trip

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ btnOne.addEventListener('click', first);
 btnTwo.addEventListener('click', noFirst);
 
 function first() {
-  image.setAttribute("src", "./style/photo3.jpeg");
+  image.setAttribute("src", "./style/photo4.jpeg");
   text.innerHTML = "What would you like to see?";
   btnOne.innerHTML = "Museum!";
   btnTwo.innerHTML = "The City!";
@@ -24,7 +24,6 @@ function museum(){
 function theCity(){
   window.open('https://www.10adventures.com/city-walks/new-york-city/');
 }
-
 
 function noFirst() {
   image.setAttribute("src", "./style/photo2.webp");
@@ -43,10 +42,13 @@ function allPopular() {
   btnTwo.innerHTML = "Trip 2!";
 
   btnOne.addEventListener('click', tripOne);
-  btnTwo.addEventListener('click', tripTwo);}
+  btnTwo.addEventListener('click', tripTwo);
+}
 
 function tripOne(){
-  window.open('https://frominwoodout.com/non-touristy-things-to-do-in-nyc/');}
+  window.open('https://www.marcofeng.com/usa/best-non-touristy-things-to-do-in-nyc');
+}
 
 function tripTwo(){
-  window.open('https://www.marcofeng.com/usa/best-non-touristy-things-to-do-in-nyc');}
+  window.open('https://frominwoodout.com/non-touristy-things-to-do-in-nyc/');
+}
